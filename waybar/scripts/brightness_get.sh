c660e1b1999e5d4e9d708517b443e15bdf626c16
aquci <archuserr@proton.me>
2026-05-20 22:10:46 +0300

    big waybar udpate latter need to fix ram display

--- waybar/scripts/brightness_get.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-VAL=$(ddcutil getvcp 10 --bus=4 2>/dev/null | grep -oP 'current value = \K[0-9]+')
-
-[ -z "$VAL" ] && VAL="--"
-
-echo $VAL
--- waybar/scripts/set-brightness.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-STEP=${1:-15}
-
-ddcutil -b 4 setvcp 10 + $STEP --noverify
-ddcutil -b 5 setvcp 10 + $STEP --noverify
