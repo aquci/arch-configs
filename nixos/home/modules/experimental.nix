b4e2a6dd657c981ffd40f94f25c83f9649c5732e
aquci <archuserr@proton.me>
2026-07-27 18:18:10 +0300

    swap documenteton from conf to here and fix extra lines

--- nixos/home/modules/experimental.nix
+++ nixos/home/modules/experimental.nix
@@ -1,12 +1,11 @@
 { pkgs, ... }:
 
 {
-
+  documentation.nixos.enable = false;
   nixpkgs.config.allowUnfree = true;
 
   nix.settings.experimental-features = [
     "nix-command"
     "flakes"
   ];
-  
 }
--- nixos/home/modules/locale.nix
+++ nixos/home/modules/locale.nix
@@ -1,6 +1,8 @@
 { ... }:
 
 {
+
   time.timeZone = "Europe/Moscow";
   i18n.defaultLocale = "en_US.UTF-8";
+  
 }
