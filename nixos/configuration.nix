0e5e31e42cfa942272b2fc809704b8a59a40a657
aquci <archuserr@proton.me>
2026-08-14 22:41:58 +0300

    update syntax

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -1,18 +1,18 @@
 {config, pkgs, ...}:
 
 {
- imports = [
-  ./hardware-configuration.nix
-  ./home/modules/services.nix
-  ./home/modules/packages.nix
-  ./home/modules/programs.nix
-  ./home/modules/user.nix
-  ./home/modules/networking.nix
-  ./home/modules/fonts.nix
-  ./home/modules/nvidia.nix
-  ./home/modules/experimental.nix
-  ./home/modules/boot.nix
-  ./home/modules/locale.nix
+  imports = [
+    ./hardware-configuration.nix
+    ./home/modules/services.nix
+    ./home/modules/packages.nix
+    ./home/modules/programs.nix
+    ./home/modules/user.nix
+    ./home/modules/networking.nix
+    ./home/modules/fonts.nix
+    ./home/modules/nvidia.nix
+    ./home/modules/experimental.nix
+    ./home/modules/boot.nix
+    ./home/modules/locale.nix
 ];
 
   security.sudo.wheelNeedsPassword = true;
